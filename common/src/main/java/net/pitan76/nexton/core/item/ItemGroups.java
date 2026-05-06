1100b51461e737b04ea27e577daf260462d510f1
PTOM76 <ptms76@outlook.jp>
2026-05-06 16:15:52 +0900

    update NI_GROUP icon to use FUEL_GENERATOR item for improved visual representation

--- common/src/main/java/net/pitan76/nexton/core/item/ItemGroups.java
+++ common/src/main/java/net/pitan76/nexton/core/item/ItemGroups.java
@@ -1,13 +1,15 @@
 package net.pitan76.nexton.core.item;
 
 import net.pitan76.mcpitanlib.api.item.CreativeTabBuilder;
+import net.pitan76.mcpitanlib.api.util.ItemStackUtil;
+import net.pitan76.nexton.machinery.item.Items;
 
 import static net.pitan76.nexton.core.NextonCore._id;
 import static net.pitan76.nexton.core.NextonCore.registry;
 
 public class ItemGroups {
 
-    public static CreativeTabBuilder NI_GROUP = CreativeTabBuilder.create(_id("main")).setIcon();
+    public static CreativeTabBuilder NI_GROUP = CreativeTabBuilder.create(_id("main")).setIcon(() -> ItemStackUtil.create(Items.FUEL_GENERATOR.get()));
 
     public static void init() {
         registry.registerItemGroup(NI_GROUP);
