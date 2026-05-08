f1db3c6ecdc54de82c64cbdab47f681748ac95ec
PTOM76 <ptms76@outlook.jp>
2026-05-08 19:26:08 +0900

    refactor item group initialization to set icon conditionally based on mod loading

--- common/src/main/java/net/pitan76/nexton/core/item/ItemGroups.java
+++ common/src/main/java/net/pitan76/nexton/core/item/ItemGroups.java
@@ -2,6 +2,7 @@ package net.pitan76.nexton.core.item;
 
 import net.pitan76.mcpitanlib.api.item.CreativeTabBuilder;
 import net.pitan76.mcpitanlib.api.util.ItemStackUtil;
+import net.pitan76.mcpitanlib.api.util.PlatformUtil;
 import net.pitan76.nexton.machinery.item.Items;
 
 import static net.pitan76.nexton.core.NextonCore._id;
@@ -9,9 +10,12 @@ import static net.pitan76.nexton.core.NextonCore.registry;
 
 public class ItemGroups {
 
-    public static CreativeTabBuilder NI_GROUP = CreativeTabBuilder.create(_id("main")).setIcon(() -> ItemStackUtil.create(Items.FUEL_GENERATOR.get()));
+    public static CreativeTabBuilder NI_GROUP = CreativeTabBuilder.create(_id("main"));
 
     public static void init() {
+        if (PlatformUtil.isModLoaded("nextonmachinery"))
+            NI_GROUP.setIcon(() -> ItemStackUtil.create(Items.FUEL_GENERATOR.get()));
+
         registry.registerItemGroup(NI_GROUP);
     }
 }
