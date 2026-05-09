646609839a71484e1ad2f7febda6f3e12c1067c5
PTOM76 <ptms76@outlook.jp>
2026-05-09 18:59:52 +0900

    bump mod version to 0.0.8 and update item group icon implementation

--- common/src/main/java/net/pitan76/nexton/core/item/ItemGroups.java
+++ common/src/main/java/net/pitan76/nexton/core/item/ItemGroups.java
@@ -1,9 +1,9 @@
 package net.pitan76.nexton.core.item;
 
 import net.pitan76.mcpitanlib.api.item.CreativeTabBuilder;
-import net.pitan76.mcpitanlib.api.util.ItemStackUtil;
 import net.pitan76.mcpitanlib.api.util.PlatformUtil;
-import net.pitan76.nexton.machinery.item.Items;
+import net.pitan76.mcpitanlib.midohra.item.ItemWrapper;
+import net.pitan76.nexton.machinery.NextonMachinery;
 
 import static net.pitan76.nexton.core.NextonCore._id;
 import static net.pitan76.nexton.core.NextonCore.registry;
@@ -14,7 +14,7 @@ public class ItemGroups {
 
     public static void init() {
         if (PlatformUtil.isModLoaded("nextonmachinery"))
-            NI_GROUP.setIcon(() -> ItemStackUtil.create(Items.FUEL_GENERATOR.get()));
+            NI_GROUP.setIcon(() -> ItemWrapper.of(NextonMachinery._id("generator")).createStack().toMinecraft());
 
         registry.registerItemGroup(NI_GROUP);
     }
